8552010e77f95a233691f127ca3563461ac046fa
Vidun Nethdina <vinethdina77@gmail.com>
2023-10-12 00:26:01 +0530

    railway

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
-  return (
-    <div>Demo</div>
-  )
-}
+  
+  const [article, setArticle] = useState({
+    url:'',
+    summary:''
+  });
 
-export default Demo
+  const handleSubmit = async (e)=>{
+    alert('submit');
+  }
+  return (
+    <section className="mt-16 w-full max-w-xl flex flex-row">
+      {/* search */}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-centeritems-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            className="url_input peer"
+            type="url"
+            placeholder="Enter a url"
+            value={article.url}
+            onChange={(e) => setArticle({
+              ...article, url:e.target.value
+            })}
+            required
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus: text-gray-700"
+          >
+            ↵
+          </button>
+        </form>
+        {/* history */}
+
+      </div>
+      {/* display results */}
+    </section>
+  );
+};
+
+export default Demo;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,22 @@
-import {logo} from '../assets';
+import { logo } from "../assets";
 
 const Hero = () => {
   return (
-    <Header className="w-full flex  justify-center items-center flex-col">
-      <nav className="flex justify-between items-center flex-row w-full">
-        <img src={logo} className="w-28 object-contain"/>
-        
+    <header className="w-full flex  justify-center items-center flex-col">
+      <nav className="flex justify-between items-center flex-row w-full mb-10 pt-3">
+        <img src={logo} className="w-28 object-contain" />
+        <button type="button" className="black_btn">
+          Github
+        </button>
       </nav>
-    </Header>
-  )
-}
+      <h1 className="head_text">
+        Summerize articles with
+        <br className="max-md:hidden" />
+        <span>OpenAI GPT4</span>
+      </h1>
+      <h2 className="desc">this is testing</h2>
+    </header>
+  );
+};
 
-export default Hero
+export default Hero;
